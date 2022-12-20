5847efbdf9ab319dc1181ed58b6c1c8214890210
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-12-20 22:34:50 +0530

    Costum Exception Handleing For Invalid Mood

---
+++ MoodAnalyzerPrograms/MoodAnalyzer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerPrograms
+{
+    public class MoodAnalyzer
+    {
+        public string message;
+        //default constructor
+        public MoodAnalyzer()
+        {
+
+        }
+        //paramterized constructor
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMoodWithoutConstructor(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+        public string AnalyzeMood()
+        {
+            try
+            {
+                if (message.ToLower().Contains("sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
